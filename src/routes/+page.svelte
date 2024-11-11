a71d4630f94f4da81140fadf9e03e4899390d138
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-11-11 08:32:38 -0500

    starter

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Selection from "../components/selection.svelte";
+</script>
+
+<main>
+  <div class="selection">
+    <Selection/>
+    <Selection/>
+    <Selection/>
+    <Selection/>
+  </div>
+</main>
+
+<style>
+  .selection {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    height: 100vh;
+    padding-left: 20px;
+  }
+</style>
+
